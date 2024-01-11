d0c5f80eb1689c201304036873787623d476d129
Kevin Koech <kevinkoech@proton.me>
2024-01-11 11:38:16 +0200

    concat two string arguments

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Define a function that takes two strings as args
+and returns a concated string
+"""
+
+
+def concat(first_string: str, second_string: str) -> str:
+    """
+    Concatenates two strings
+    """
+    new_string = first_string + second_string
+    return new_string
