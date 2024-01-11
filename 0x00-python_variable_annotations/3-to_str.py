ab2d37469d5feb61f824be4ae9a63067711f06f1
Kevin Koech <kevinkoech@proton.me>
2024-01-11 11:50:42 +0200

    takes a float and returns its string representation

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+Define a function that takes a float n as argument
+and returns the string representation of the float
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Takes a float and returns its str representation
+    """
+    return str(n)
