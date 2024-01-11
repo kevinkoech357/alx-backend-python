6ac1b88e925fc81d16932a1f57d4708de1fc2811
Kevin Koech <kevinkoech@proton.me>
2024-01-11 11:44:40 +0200

    floor a float and return value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Takes a float n as arg
+and returns the floor
+"""
+
+import math
+
+
+def floor(n: float) -> float:
+    """
+    Floor the arg n and returns its floor
+    """
+    return math.floor(n)
