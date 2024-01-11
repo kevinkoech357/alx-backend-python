3246ca130a0f14e3aebcf1425bb790199d4ef5c7
Kevin Koech <kevinkoech@proton.me>
2024-01-11 11:30:13 +0200

    add two float numbers and return their sum

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+""""
+Define a function that takes two float args
+and return their sum
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Add 2 floats and return their sum
+    """
+    total: float = a + b
+    return total
