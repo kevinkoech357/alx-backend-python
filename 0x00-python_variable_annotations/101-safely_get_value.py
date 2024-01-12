8efbcfd0b3213887a76882a9ed2181f040d713ef
Kevin Koech <kevinkoech@proton.me>
2024-01-12 22:50:29 +0300

    fix failing checks

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -6,7 +6,7 @@ based on the key
 """
 
 
-from typing import TypeVar, Any, Mapping, Union
+from typing import TypeVar, Any, Mapping, Union, Optional
 
 
 T = TypeVar('T')
@@ -14,7 +14,7 @@ T = TypeVar('T')
 
 def safely_get_value(
         dct: Mapping, key: Any, default:
-        Union[Any, None] = None) -> Union[Any, T]:
+        Optional[T] = None) -> Union[Any, T]:
     """
     Retrieve the value from the dictionary for the given key.
     """
--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,22 +4,22 @@
 Validating with mypy.
 """
 
-from typing import Tuple, Any, List
+from typing import List, Tuple
 
 
-from typing import List, Tuple, Any
-
-
-def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
     """
     Zooms in the input tuple by repeating each element by the specified factor.
     """
-    zoomed_in: List[Any] = [item for item in lst for _ in range(factor)]
-    return tuple(zoomed_in)
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
+    ]
+    return zoomed_in
 
 
-array: Tuple[int, int, int] = (12, 72, 91)
+array = (12, 72, 91)
 
-zoom_2x: Tuple[Any, ...] = zoom_array(array)
+zoom_2x = zoom_array(array)
 
-zoom_3x: Tuple[Any, ...] = zoom_array(array, 3)
+zoom_3x = zoom_array(array, 3)
