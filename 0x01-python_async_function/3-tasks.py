8babbd0cd7407d528e277acd825bf17110154726
Kevin Koech <kevinkoech@proton.me>
2024-01-15 18:36:43 +0300

    change return type from Any to asyncio.Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,12 +6,12 @@ Returns asyncio.Task
 
 
 import asyncio
-from typing import Any
+#from typing import Any
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
     Returns asyncio.Task
     """
