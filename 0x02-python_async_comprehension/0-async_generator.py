967011bbe35699cd0e459e4a46f174b57797dd6e
Kevin Koech <kevinkoech@proton.me>
2024-01-16 15:33:03 +0300

    add async_generator function that loops through numbers 0-10 and sleeps for 1 sec in between

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Define a coroutine called async_generator that takes no arguments.
+
+The coroutine will loop 10 times, each time asynchronously wait 1 second,
+then yield a random number between 0 and 10
+"""
+
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Yield a random value between 0 - 10
+    and sleep for a sec in between.
+    """
+    for _ in range(10):
+        i = random.uniform(0, 10)
+        yield i
+        await asyncio.sleep(1)
